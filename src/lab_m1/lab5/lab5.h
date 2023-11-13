3156f72d1f18c4ee61bc9050b3a4d52c3b4cd7bf
Rares Croicia <rarescroicia@gmail.com>
2023-11-13 18:06:15 +0200

    Add lab5
    Signed-off-by: Rares Croicia <rarescroicia@gmail.com>

--- src/lab_m1/lab5/lab5.h
+++ src/lab_m1/lab5/lab5.h
@@ -34,6 +34,9 @@ namespace m1
         implemented::Camera *camera;
         glm::mat4 projectionMatrix;
         bool renderCameraTarget;
+        bool projectionType;
+
+        GLfloat right, left, top, bottom, fov, zNear, zFar;
 
         // TODO(student): If you need any other class variables, define them here.
 
